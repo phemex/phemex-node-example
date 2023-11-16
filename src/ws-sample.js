df579d106ea1af321a9dbc7509deda878e58d68f
logan <logan.zhao@cmexpro.com>
2023-11-16 15:49:10 +0800

    feat: :sparkles: update ws-sample demos

--- src/ws-sample.js
+++ src/ws-sample.js
@@ -8,74 +8,42 @@ const OPEN = 1;
 const CLOSING = 2;
 const CLOSED = 3;
 
-(function main() {
-  const ws = new WebSocketClient(URLS.WS_URL);
-  let counter = 1;
+const ws = new WebSocketClient(URLS.WS_URL);
+const UnsubscribeTime = 5000;
 
-  function nextId() {
-    return counter++;
+let counter = 1;
+function nextId() {
+  return counter++;
+}
+
+function send(msg, callback = null) {
+  if (ws.readyState === OPEN) {
+    msg.id = msg.id || nextId();
+    if (callback) {
+      function handleMessage(e) {
+        const data = JSON.parse(e.data);
+        if (data.id === msg.id) {
+          ws.removeEventListener('message', handleMessage);
+          callback(null, data);
+        }
+      }
+
+      ws.addEventListener('message', handleMessage);
+    }
+    const msgStr = JSON.stringify(msg);
+    ws.send(msgStr);
+    console.log('\x1b[36m%s\x1b[0m', '[SEND MSG]', msgStr);
+    return;
   }
 
-  function send(msg, callback = null) {
-    if (ws.readyState === OPEN) {
-      msg.id = msg.id || nextId();
-      if (callback) {
-        function handleMessage(e) {
-          const data = JSON.parse(e.data);
-          if (data.id === msg.id) {
-            ws.removeEventListener('message', handleMessage);
-            callback(null, data);
-          }
-        }
-        ws.addEventListener('message', handleMessage);
-      }
-      const msgStr = JSON.stringify(msg);
-      ws.send(msgStr);
-      console.log('[SEND MSG]', msgStr);
-      return;
-    }
+  callback('websocket not open');
+}
 
-    callback('websocket not open');
-  }
+ws.addEventListener('message', function(e) {
+  console.log('\x1b[33m%s\x1b[0m', '[RECEIVE MSG]', e.data);
+});
 
-  startHeartbeat(ws, send);
-
-  ws.addEventListener('open', function() {
-    send({method: 'orderbook.subscribe', params: ['BTCUSD']}, function(error, data) {
-      if (error) {
-        console.log('subscribe BTCUSD orderbook failed');
-      }
-      if (data) {
-        console.log('subscribe BTCUSD orderbook succeed');
-      }
-    });
-  });
-
-  wsLogin(ws, send, function handleLogin(error, data) {
-    if (error) {
-      console.log('ws auth failed');
-    }
-
-    if (data) {
-      console.log('ws auth succeed');
-
-      send({method: 'aop.subscribe', params: []}, function(error, data) {
-        if (error) {
-          console.log('subscribe aop data failed');
-        }
-        if (data) {
-          console.log('subscribe aop data succeed');
-        }
-      });
-    }
-  });
-
-  ws.addEventListener('message', function(e) {
-    console.log('[RECEIVE MSG]', e.data);
-  });
-})();
-
-function startHeartbeat(ws, send) {
+function startHeartbeat() {
   let timer = 0;
   ws.addEventListener('open', function() {
     timer = setInterval(() => {
@@ -89,7 +57,26 @@ function startHeartbeat(ws, send) {
   });
 }
 
-function wsLogin(ws, send, callback) {
+function subscribeOrderbook(callback) {
+  ws.addEventListener('open', function() {
+    send({method: 'orderbook.subscribe', params: ['BTCUSD']}, function(error, data) {
+      if (error) {
+        console.log('subscribe BTCUSD orderbook failed');
+      }
+      if (data) {
+        console.log('subscribe BTCUSD orderbook succeed');
+      }
+
+      callback && callback();
+    });
+  });
+}
+
+function unsubscribeOrderbook() {
+  send({method: 'orderbook.unsubscribe', params: ['BTCUSD']});
+}
+
+function wsLogin(callback) {
   ws.addEventListener('open', function() {
     // auth
     const expiry = Math.floor(Date.now() / 1000) + 2 * 60;
@@ -98,3 +85,70 @@ function wsLogin(ws, send, callback) {
     send({method: 'user.auth', params: ['API', config.api_key, signature, expiry]}, callback);
   });
 }
+
+function subscribeAop(callback) {
+  send({method: 'aop.subscribe', params: []}, function(error, data) {
+    if (error) {
+      console.log('subscribe aop data failed');
+    }
+    if (data) {
+      console.log('subscribe aop data succeed');
+    }
+    callback && callback();
+  });
+}
+
+function unsubscribeAop() {
+  send({method: 'aop.unsubscribe', params: []});
+}
+
+function subscribeWo(callback) {
+  send({method: 'wo.subscribe', params: []}, function(error, data) {
+    if (error) {
+      console.log('subscribe wo data failed');
+    }
+    if (data) {
+      console.log('subscribe wo data succeed');
+    }
+    callback && callback();
+  });
+}
+
+function unsubscribeWo() {
+  send({method: 'wo.unsubscribe', params: []});
+}
+
+/**
+ * testDemos:
+ * 1. heartbeat
+ * 2. public subscribe/unsubscribe (orderbook)
+ * 3. auth
+ * 4. private subscribe/unsubscribe (account-order-position)
+ * 5. private subscribe/unsubscribe (wallet-order)
+ */
+function testDemos() {
+  // 1. heartbeat
+  startHeartbeat();
+
+  // 2. public subscribe/unsubscribe (orderbook)
+  subscribeOrderbook(() => setTimeout(() => unsubscribeOrderbook, UnsubscribeTime));
+
+  // 3. auth
+  wsLogin(function handleLogin(error, data) {
+    if (error) {
+      console.log('ws auth failed');
+    }
+
+    if (data) {
+      console.log('ws auth succeed');
+
+      // 4. private subscribe/unsubscribe (account-order-position)
+      subscribeAop(() => setTimeout(unsubscribeAop, UnsubscribeTime));
+
+      // 5. private subscribe/unsubscribe (wallet-order)
+      subscribeWo(() => setTimeout(() => unsubscribeWo, UnsubscribeTime))
+    }
+  });
+}
+
+testDemos();
